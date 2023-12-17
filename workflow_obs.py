364d1c2d9408f9ba329912f9974473369b4ed2e5
Marco Braun <Braun.Marco@ouranos.ca>
2023-12-17 18:48:07 -0500

    Dots on Maps prototype working for temperature, trends included

--- workflow_obs.py
+++ workflow_obs.py
@@ -5,6 +5,7 @@ import os
 
 import xarray as xr
 import numpy as np
+import xarray.plot
 from dask import config as dskconf
 from dask.distributed import Client
 from dask.diagnostics import ProgressBar
@@ -322,129 +323,209 @@ if __name__ == "__main__":
         # get input and iter
         dict_input = pcat.search(**CONFIG["plotting"]["data"]).to_dataset_dict(**tdd)
         for key_input, ds_input in sorted(dict_input.items()):
-            with (
-                Client(**CONFIG["plotting"]["dask"], **daskkws),
-                xs.measure_time(name=f"plotting {key_input}", logger=logger),
-            ):
+            with (Client(**CONFIG["plotting"]["dask"], **daskkws),
+                  xs.measure_time(name=f"plotting {key_input}", logger=logger)):
+
                 # practice with monthly or seasonal data
                 # if 'AS-JAN' in key_input: continue
-                if 'AHCCD' in key_input: continue
+                # if any(i for i in ['QS', 'MS'] if i in key_input): continue  # ToDo: remove this
+                if 'AHCCD' in key_input:  # currently AHCCD data are only plotted on top of reanalyis
+                    continue
 
                 import cartopy.crs as ccrs
                 import matplotlib.pyplot as plt
                 import figanos.matplotlib as fg
+                from textwrap import wrap
                 from pathlib import Path
+
+                import matplotlib
+                # matplotlib.use('wxAgg')
+                # matplotlib.use("TkAgg")
+                # matplotlib.use("macosx")
+                # matplotlib.use('Qt5Agg')
 
                 fg.utils.set_mpl_style('ouranos')
                 projection = ccrs.LambertConformal()
-                freq = CONFIG['plotting']['xrfreq'][ds_input.attrs['cat:xrfreq']]
-                adj = {'year': 'annual', 'month': 'monthly', 'season': 'seasonal'}
-                freq_adj = adj[freq]
+                freq = CONFIG['plotting']['xrfreq_names'][ds_input.attrs['cat:xrfreq']]
 
-                for period in ds_input.period.values:  #['1951-1980']:
-                    for ind in ds_input.data_vars.values():
+                for period in ds_input.period.values:  # ['1951-1980']:  #   #  # FixMe: remove this
+                    for da_grid in ds_input.data_vars.values():
 
-                        plot_id = (f"{CONFIG['plotting'][ind.name]['label']} "
-                                   f"{ind.attrs['long_name'].lower()} - "
+                        # if not all(i in da_grid.name for i in ['tg_mean_clim_mean', 'tg']): continue  # Todo: remove this
+
+                        # get the corresponding AHCCD observation data -----------------------------------------
+                        da_station = pcat.search(
+                            source='AHCCD',
+                            processing_level='climatology',
+                            xrfreq=ds_input.attrs.get('cat:xrfreq')
+                        ).to_dataset()[da_grid.name]
+
+                        # get a plot_id for labeling and file naming -------------------------------------------
+                        plot_id = (f"{CONFIG['plotting'][da_grid.name]['label']} "
+                                   f"{da_grid.attrs['long_name'].lower()} - "
                                    f"{ds_input.attrs['source']} ({period})")
+                        # print(f"Raw ID: ..................... {da_grid.name} --- {plot_id}")
 
-                        # print(f"Raw ID: ..................... {ind.name} --- {plot_id}")
-                        # trim the plot_id
+                        # trim the plot_id ---------------------------------------------------------------------
+                        adj = {'year': 'annual', 'month': 'monthly', 'season': 'seasonal'}
                         changes = {
                             'interannual 30-year climatological': 'interannual',
                             'intra 30-year climatological average of ': 'intra-',
-                            '30-year climatological average': f'{freq_adj} climate average',
+                            '30-year climatological average': f'{adj[freq]} climate average',
                             '30-year climatological linregress': 'linear climate trend',
                             'total standard deviation': 'climate standard deviation',
                             '.': '',
                         }
                         for k, v in changes.items():
                             plot_id = plot_id.replace(k, v).strip()
-                        logger.info(f"Variable: {ind.name} --- Plotting {plot_id}")
-                        # print(f'Trimmed ID: {ind.name} --- {plot_id}')
+                        logger.info(f"Variable: {da_grid.name} --- Plotting {plot_id}")
+                        # print(f'Trimmed ID: {da_grid.name} --- {plot_id}')
 
-                        # skip trends for now
-                        # if 'linregress' in ind.name: continue
-
-                        # convert units ToDo: this should be done in the clean-up step
+                        # convert units ToDo: this should be done in the clean-up step -------------------------
                         from xclim.core import units
                         try:
-                            ind = units.convert_units_to(ind, CONFIG["plotting"][ind.name]["units"])
+                            da_grid = units.convert_units_to(da_grid, CONFIG["plotting"][da_grid.name]["units"])
+                            if '_mean_clim_mean' in da_station.name:  # Fixme this is so dirty! Because not all in ˚C
+                                da_station = units.convert_units_to(da_station, CONFIG["plotting"][da_station.name]["units"])
                         except (KeyError, ValueError):
                             pass
 
-                        # selection and scaling of data ToDo: the scaling should be done in the clean-up step
+                        # selection and scaling of data ToDo: the scaling should be done in the clean-up step --
                         sel_kwargs = {"period": period,
-                                      freq: ind[freq].values}
+                                      freq: da_grid[freq].values}
                         scale_factor = 1
-                        if 'linregress' in ind.name:
+                        if 'linregress' in da_grid.name:
                             sel_kwargs.setdefault('linreg_param', 'slope')
                             scale_factor = 10
 
-                        # use_attrs
-                        use_attrs = {}  # {"suptitle": plot_id, }
+                        # use_attrs -----------------------------------------------------------------------------
+                        use_attrs = {}  # {'suptitle': plot_id, 'title': ''}  # {"suptitle": plot_id, }
 
-                        # levels and ticks
-                        if 'linspace' in CONFIG["plotting"][ind.name]["ticks"]:
-                            levels = np.linspace(CONFIG["plotting"][ind.name]["limits"][freq]["vmin"],
-                                                 CONFIG["plotting"][ind.name]["limits"][freq]["vmax"],
-                                                 CONFIG["plotting"][ind.name]["levels"])
+                        # figure arguments -
+                        fig_kwargs = {'figsize': (15, 21)}  # 'layout': 'tight', 'dpi': 300}  # 'layout': 'tight'
+
+                        # levels and ticks -----------------------------------------------------------------------
+                        if 'linspace' in CONFIG["plotting"][da_grid.name]["ticks"]:
+                            levels = list(np.linspace(CONFIG["plotting"][da_grid.name]["limits"][freq]["vmin"],
+                                                 CONFIG["plotting"][da_grid.name]["limits"][freq]["vmax"],
+                                                 CONFIG["plotting"][da_grid.name]["levels"]))
                             ticks = levels[0::2]
                         else:
-                            levels = CONFIG["plotting"][ind.name]["levels"]
-                            ticks = CONFIG["plotting"][ind.name]["ticks"]
+                            levels = CONFIG["plotting"][da_grid.name]["levels"]
+                            ticks = CONFIG["plotting"][da_grid.name]["ticks"]
 
-                        # plot kwargs
-                        plot_kwargs = {"x": "lon",
-                                       "y": "lat",
-                                       "col": None if 'year' in freq else freq,
-                                       "col_wrap": CONFIG["plotting"]["col_wrap"][freq],
-                                       **CONFIG["plotting"][ind.name]["limits"][freq],
-                                       "cbar_kwargs": {
-                                           "shrink": 0.8,
-                                           "ticks": ticks,
-                                            },
-                                       }
+                        # print(f'Freq: {freq}\nLevels: {levels}\nTicks: {ticks}\n'
+                        #       f'{CONFIG["plotting"][da_grid.name]["limits"][freq]}')  # FixMe: remove this
 
-                        # remove col and col_wrap if annual, gridmap doesn't like it ToDo: fix in fg.gridmap
+                        # plot kwargs ----------------------------------------------------------------------------
+                        plot_kwargs_grid = {
+                            "x": "lon",
+                            "y": "lat",
+                            "col": None if 'year' in freq else freq,
+                            "col_wrap": CONFIG["plotting"]["col_wrap"][freq],
+                            **CONFIG["plotting"][da_grid.name]["limits"][freq],
+                            "cbar_kwargs": {
+                                "shrink": 0.4,
+                                "ticks": ticks,
+                            },
+                        }
+
+                        # remove col and col_wrap if annual, gridmap doesn't like it ToDo: fix in fg.gridmap? ----
                         if 'year' in freq:
-                            plot_kwargs.pop('col')
-                            plot_kwargs.pop('col_wrap')
+                            plot_kwargs_grid.pop('col')
+                            plot_kwargs_grid.pop('col_wrap')
 
-                        # gridmap kwargs
-                        gridmap_kwargs = {"projection": projection,
-                                          "transform": ccrs.PlateCarree(),
-                                          "features": ['states'],
-                                          "contourf": True,
-                                          "divergent": CONFIG["plotting"][ind.name]["divergent"],
-                                          "levels": levels,
-                                          "frame": True,
-                                          }
+                        # gridmap kwargs -------------------------------------------------------------------------
+                        gridmap_kwargs = {
+                            "projection": projection,
+                            "transform": ccrs.PlateCarree(),
+                            "features": {'states': {'edgecolor': 'dimgray', 'linewidth': 0.3}},
+                            "contourf": True,
+                            "divergent": CONFIG["plotting"][da_grid.name]["divergent"],
+                            "levels": levels,
+                            "frame": True,
+                        }
 
+                        # let's get to work plotting -------------------------------------------------------------
                         with xr.set_options(keep_attrs=True):
-                            fg.gridmap(data=ind.sel(sel_kwargs) * scale_factor,
-                                       plot_kw=plot_kwargs,
-                                       use_attrs=use_attrs,
-                                       **gridmap_kwargs,
-                                       )
+                            fax = fg.gridmap(
+                                data=da_grid.sel(sel_kwargs) * scale_factor,
+                                plot_kw=plot_kwargs_grid,
+                                fig_kw=fig_kwargs,
+                                **gridmap_kwargs,
+                            )
+
+                            # add station data -
+                            plot_kwargs_station = {
+                                                      k: plot_kwargs_grid[k]
+                                                      for k in ['x', 'y', 'vmin', 'vmax']
+                                                  } | {
+                                                      'edgecolor': 'dimgray',
+                                                      'linewidth': 0.01,
+                                                      'add_colorbar': False,
+                                                      'label': 'AHCCD-Stations'
+                                                  }
+                            scattermap_kwargs = {k: gridmap_kwargs[k] for k in
+                                                 ['transform', 'divergent', 'levels', 'frame']}
+
+                            for ax, sel_kwarg in zip(
+                                    fax.axs.flat if isinstance(fax, xarray.plot.FacetGrid) else [fax],
+                                    [{}] if freq in 'year' else [{freq: f} for f in da_station[freq].values]):
+                                title = ax.get_title()
+                                data = da_station.sel(sel_kwargs | sel_kwarg) * scale_factor
+                                data = data.sel({data.dims[0]: ~np.isnan(data.squeeze().values)})  # take out nans # ToDo: The squeeze can be removed when data were regenergated without the year dimension
+                                scax = fg.scattermap(
+                                    # data=xr.Dataset({da_station.name: da_station.sel(sel_kwargs) * scale_factor}),
+                                    data=data,
+                                    ax=ax,
+                                    plot_kw=plot_kwargs_station,
+                                    **scattermap_kwargs,
+                                    # cmap=[c.cmap for c in ax.get_children() if hasattr(c, 'cmap')][0],
+                                )
+                                scax.set_title('')
+                                scax.text(x=0.83, y=0.93, s=title, fontsize=14, fontweight='bold', transform=ax.transAxes)
+                                scax.set_extent([-79.5, -60, 45, 61], crs=ccrs.PlateCarree())
+                            scax.legend(loc='lower left',
+                                        fontsize=16,
+                                        bbox_to_anchor=(1.08, 1.0),
+                                        edgecolor='dimgray',
+                                        frameon=True
+                                        )
+                            # Todo: Try fig.legend(...)
+                            # fax.fig.legend(['A', 'B', 'C'],
+                            #                loc='outside center right',
+                            #                fontsize=16,
+                            #                edgecolor='red',
+                            #                frameon=True)
+                            fig = fax.fig if isinstance(fax, xarray.plot.FacetGrid) else fax.axes.figure
+                            fig.subplots_adjust(left=0.01,
+                                                    right=0.79,
+                                                    top=0.92,
+                                                    bottom=0.01,
+                                                    wspace=0.0,
+                                                    hspace=0.0)
+                            fig.suptitle('\n'.join(wrap(plot_id[:1].upper() + plot_id[1:], 50)),
+                                             fontsize=26, fontweight='bold', y=0.96)
+                            #fig.tight_layout()
                         # plt.show(block=False)
 
-                        # prepare file_name
+                        # prepare file_name, directory and save -------------------------------------------------
                         changes = {'standard deviation': 'std', '- ': '', '(': '', ')': '', ' ': '_', }
                         file_name = plot_id
                         for k, v in changes.items():
                             file_name = file_name.replace(k, v)
                         cur = {
-                            "processing_level": "figures",
+                            "processing_level": "test_figures",
                             "period": period,
                             "file_name": file_name,
                         }
                         out_file = Path(f"{CONFIG['paths']['figures']}".format(**cur))
-                        if not out_file.parent.exists(): out_file.parent.mkdir(parents=True, exist_ok=True)
+                        if not out_file.parent.exists():
+                            out_file.parent.mkdir(parents=True, exist_ok=True)
 
                         # save to png
                         logger.info(f"Saving {out_file}.png ...")
-                        plt.savefig(out_file, dpi=200, bbox_inches='tight')
+                        fig.savefig(out_file, dpi=200, bbox_inches='tight')
                         # print(f"Saving {out_file}.png ...")
 
     # --- ENSEMBLES ---
